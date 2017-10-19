4156b335889988e7fe3988046835f9bdfc3cbdc2
Gugelhupf <gokan.ekinci.pro@outlook.com>
2017-10-19 09:46:11 +0200

    feature: Initial Commit

---
+++ client-management-services/src/main/java/fr/ekinci/clientmanagement/user/models/UserDto.java
@@ -0,0 +1,14 @@
+package fr.ekinci.clientmanagement.user.models;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author Gokan EKINCI
+ */
+@Data
+@ToString
+public class UserDto {
+	private String lastName;
+	private String firstName;
+}
